c78f8d0e59e09adfe461151e5dd149057a262971
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2023-08-01 17:46:13 +0200

    Refactor grass lighting calculation

--- Assets/Shaders/Source/GeometryGrassForwardPass.hlsl
+++ Assets/Shaders/Source/GeometryGrassForwardPass.hlsl
@@ -24,6 +24,28 @@ VertexOutput VSMain(in Attributes input)
 
 float4 PSMain(in GeometryOutput input) : SV_Target
 {
+#define NEW_PATH 1
+#if NEW_PATH
+    InputData inputData = (InputData) 0;
+    inputData.positionWS = input.positionWS;
+    inputData.normalWS = NormalizeNormalPerPixel(input.normalWS);
+    inputData.viewDirectionWS = GetWorldSpaceNormalizeViewDir(input.positionWS);
+    inputData.shadowCoord = TransformWorldToShadowCoord(input.positionWS);
+    
+    half shadowAttenuation = saturate(MainLightRealtimeShadow(inputData.shadowCoord) + 0.05f);
+    Light mainLight = GetMainLight();
+    inputData.bakedGI = max(mainLight.color.rgb, 0.01) * shadowAttenuation;
+    
+    SurfaceData surface = (SurfaceData) 0;
+    surface.albedo = lerp(_BaseColor, _TipColor, input.uv.y);
+    surface.alpha = 1;
+    surface.metallic = 0;
+    surface.specular = 0;
+    surface.occlusion = 1;
+    surface.normalTS = half3(0, 0, 1);
+    
+    return UniversalFragmentPBR(inputData, surface);
+#else
     float4 bladeTint = tex2D(_BladeTexture, input.uv);
 
 #ifdef MAIN_LIGHT_CALCULATE_SHADOWS
@@ -34,9 +56,10 @@ float4 PSMain(in GeometryOutput input) : SV_Target
 	bladeTint *= shadowColor;
 
 	Light light = GetMainLight();
-	bladeTint *= float4(max(light.color.xyz, 0.01), 1);
+	bladeTint *= float4(max(light.color.rgb + 0.75f, 0.01f), 1);
 #endif
 
     return lerp(_BaseColor, _TipColor, input.uv.y) * bladeTint;
+#endif
 }
 #endif
